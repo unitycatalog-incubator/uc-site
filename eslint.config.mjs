5aae653f49989097af720a30a22a2dacf54f4b88
Jake Bellacera <hi@jakebellacera.com>
2025-04-29 22:08:29 +0000

    Add prettier and eslint support for Astro

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
   {
@@ -14,11 +16,13 @@ export default [
       "pnpm-workspace.yaml",
     ],
   },
-  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   importPlugin.flatConfigs.recommended,
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       "import/no-named-as-default-member": "off",
