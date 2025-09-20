f792e17b9b9b30a20584fd541e7a6c27163b0c61
Jake Bellacera <hi@jakebellacera.com>
2025-09-20 14:08:03 -0700

    Add section component to blog index

--- packages/uc-theme/src/components/index.ts
+++ packages/uc-theme/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Nav } from "./Nav.astro";
 export { default as Button } from "./Button.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Search } from "./Search.astro";
+export { default as Section } from "./Section.astro";
