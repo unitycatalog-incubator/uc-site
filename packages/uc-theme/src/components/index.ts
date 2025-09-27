b8fa8cb57cfba0127c2a15c38b392b8cbb700473
Jake Bellacera <hi@jakebellacera.com>
2025-09-27 11:44:01 -0700

    Clean up blog pages, add input and cleanup Button

--- packages/uc-theme/src/components/index.ts
+++ packages/uc-theme/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Card } from "./Card.astro";
 export { default as PageLayout } from "./PageLayout.astro";
+export { default as Input } from "./Input.astro";
 export { default as Nav } from "./Nav.astro";
 export { default as Button } from "./Button.astro";
 export { default as Footer } from "./Footer.astro";
